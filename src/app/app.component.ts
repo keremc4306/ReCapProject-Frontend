77f6901c582fa6caefb4e78c99ff62e2369f2489
Kerem Candangil <keremc4306@gmail.com>
2021-04-10 20:11:13 +0300

    Component, model ve service dosyaları oluşturuldu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,47 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'carrental';
-  user: string = 'Kerem Candangil';
-  product = {
-    productId: 1,
-    productName: 'bardak',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product2 = {
-    productId: 2,
-    productName: 'laptop',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product3 = {
-    productId: 3,
-    productName: 'mouse',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product4 = {
-    productId: 4,
-    productName: 'keyboard',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product5 = {
-    productId: 5,
-    productName: 'kamera',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-
-  products = [
-    this.product,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
+  title = 'rent-a-car';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    CarComponent,
+    ColorComponent,
+    CustomerComponent,
+    NaviComponent,
+    RentalComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
